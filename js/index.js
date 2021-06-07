14211190944e67f38d7fa86e900e475786877c76
Rohanaj60 <53361907+Rohanaj60@users.noreply.github.com>
2021-06-07 13:25:10 +0545

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -2,13 +2,28 @@ $(document).ready(function()
 {
 $(window).bind('mousewheel', function(event) {
   if (event.originalEvent.wheelDelta >= 0) {
-      console.log('Scroll up');
-      $(".nav-bar").show();
+      // console.log('Scroll up');
+$(".nav-bar").show();
       // $(".nav-bar").addClass("nav-jquery");
   }
   else {
-      console.log('Scroll down');
-      $(".nav-bar").hide();
+      // console.log('Scroll down');
+   $(".nav-bar").hide();
   }
 });
 });
+
+
+$(document).ready(function(){
+$(".ham-menu1").click(function(){
+$(".ham-menu").toggle();
+
+
+});
+});
+$(document).ready(function(){
+  $(".ham-links").click(function(){
+  $(".ham-menu").hide();
+  
+  });
+  });
